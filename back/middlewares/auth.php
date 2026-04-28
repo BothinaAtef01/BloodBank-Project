e93c727cde5d9d345db0f2791ed6d7c153a64d09
Awrad Tarig <werfoor@gmail.com>
2026-04-28 18:59:10 +0300

    composer install

--- back/middlewares/auth.php
+++ back/middlewares/auth.php
@@ -2,7 +2,7 @@
 // middleware/auth.php — JWT authentication & role/permission guards
 
 require_once __DIR__ . '/../config/db.php';
-//require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 //composer install
 use Firebase\JWT\JWT;
